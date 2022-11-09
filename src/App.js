348985013bdb7fa315b3dce7bf9bfde9ed4b83a6
janekigenyi <janekigenyi@gmail.com>
2022-11-09 19:46:14 +0300

    created three components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
+import React, { useState } from "react";
+
+import './App.css';
+import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 
-function App () {
-	return (
-		<div className="App">
-			<Register />
-		</div>
-	);
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
 }
 
 export default App;
