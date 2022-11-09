24050f9a85c43d0a7e8affad422e076b552eadf3
janekigenyi <janekigenyi@gmail.com>
2022-11-09 20:10:08 +0300

    added props and use state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import React, { useState } from "react";
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
@@ -9,6 +10,20 @@ export const Login = (props) => {
         console.log(email);
     }
 
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
